d458aa547b0848ea667a97461cc06d1b76e4684d
Ina <dlsdk0601@gmail.com>
2021-07-22 11:03:10 +0900

    project renual

---
+++ src/common.js
@@ -0,0 +1,39 @@
+$(function(){
+
+    $("header").load("inc.html header > div", function(){
+        
+        // burger click
+        $("header .trigger > a").on("click", function(){
+            $(this).toggleClass("active-7");
+            $(this).toggleClass("type7");
+            $(".pop").toggleClass("active");
+            $("header h1").toggleClass("hidden");
+            $("header .topicons").toggleClass("hidden");
+        });
+
+        //menu active
+        $("#bigMenu .menu").on("click", function(){
+            $("#bigMenu .menu section").removeClass("active");
+
+            $("#bigMenu .menu section").eq( $(this).index() ).addClass("active");
+                
+        });
+        
+        //login popup
+        $("header .topicons > a:nth-of-type(1)").on("click", function(e){
+            e.preventDefault();
+            $("header .login").toggleClass("active");
+        })
+        $("header .login .close").on("click", (e) => {
+            e.preventDefault();
+            $("header .login").toggleClass("active");
+        })
+    });
+    
+    $("footer").load("inc.html footer > div");
+
+});
+
+
+
+
