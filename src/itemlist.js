60ace2659a27696040ec6e351291b0c5b9b90082
Ina <dlsdk0601@gmail.com>
2021-07-27 00:58:31 +0900

    sorting Function Finish :)

--- src/itemlist.js
+++ src/itemlist.js
@@ -5,7 +5,7 @@ fetch("data/data_items.json")
 
 const callback = (data)  => {
     
-    //mainMenu event
+    //mainMenu css event
     $("#top .downArrow").on("click", function(e){
         e.preventDefault();
         $(".sort1").toggleClass("active");
@@ -20,75 +20,75 @@ const callback = (data)  => {
     //Json-data in Array
     const items = data.item.slice();
     let selectedMenu = data.item.slice();
-    // data.item.map(item => items.push(item));
-    // data.item.map(item => selectedMenu.push(item));
     
     
     //items display function
     let itemList = "";
     let subMenuList = new Array();
+    let lowPriceItems = new Array();
+    let highPriceItems = new Array();
+    let bestItems = new Array();
 
-    function display(arr, value){
-        if(value == "all"){
-            itemList = arr.map(item => createHTMLString(item)).join("");
-            $(".mainlist .items").html(itemList);   
+    function display(arr, sort){
+        if(sort == "All"){
+            println(arr); 
+        }else if(sort == "Low price"){
+            lowPriceItems = arr.slice();
+            lowPriceItems.sort(lowSorting);
+            println(lowPriceItems);
+            lowPriceItems = [];
+        }else if(sort == "High price"){
+            highPriceItems = arr.slice();
+            highPriceItems.sort(highSorting);
+            println(highPriceItems);
+            highPriceItems = [];
+        }else if(sort == "Best"){
+            bestItems = arr.filter(item => item.popular == "best");
+            println(bestItems);
+            bestItems = [];
         }
-        else{
-            selectedMenu = [];
-            selectedMenu = arr.filter( item => item.type == value);
-            itemList = selectedMenu.map(item => createHTMLString(item)).join("");
-            $(".mainlist .items").html(itemList); 
-            selectedMenu.map( item => subMenuList.push( item.sort ) );
-        }
+        
         itemList = "";
     }
-
-    //li tag return.
+    
+    //create HTML Tag
     function createHTMLString(item){
         return `<li>
                     <figure>
-                        <a href="detail.html"><img src="${item.thum}" alt=""></a>
+                        <a href=""><img src="${item.thum}" alt=""></a>
                         <a href="detail.html">
                             <p>${item.name}</p><p>${item.price}￦</p>
                         </a>
                     </figure>
                 </li>`;
     }
+    //print function
+    function println(arr){
+        itemList = arr.map(item => createHTMLString(item)).join("");
+        $(".mainlist .items").html(itemList); 
+    }
 
     //default Page
-    display( items, "all");
+    display( items, "All" );
     
-    //submenu cange function
+    //submenu change function
     let tagList = " ";
+   
     function textChange(){
         $("#top .submenu").empty();
         const set = Array.from(new Set(subMenuList));
-
         for(let i = 0; i < set.length; i++){
-            tagList += `<span>${set[i]}</span>`;
+            tagList += `<span data-sub="${set[i]}" active="0">${set[i]}</span>`;
         }
         $("#top .submenu").html(tagList);
         tagList =" ";
         subMenuList.splice(0, subMenuList.length);
     }
 
-    //메뉴 클릭 이벤트
-    $("#top .menulist ul li").on("click", function(){
-        //Menu name, title change
-        const txt = $(this).text();
-        const lowerTxt = txt.toLowerCase();
-        $("#top .menulist p").text(txt);
-        $(".title").text(txt);
-
-        //itemlist change
-        $(".mainlist .items").empty();
-        display( items, lowerTxt);
-        textChange()
-        $(".sort1").removeClass("active");
-    });
+    
     
     //sort2 click event
-    let sortName = "";
+    let sortName = "All";
     $("#sort2 img").on("click", function(){
         $("#sort2 ul").toggleClass("hidden");
     });
@@ -116,43 +116,67 @@ const callback = (data)  => {
         }
         return a.price < b.price ? 1 : -1
     }
-    //해당하는 분류로 재분류
-    // lowPriceItems.sort(lowSorting);
-    // highPriceItems.sort(highSorting);
     
-    //분류하는 함수
-    //sort display
-    let lowPriceItems = new Array();
-    let highPriceItems = new Array();
-    let bestItems = new Array();
-    $("#sort2 ul li").on("click", function(){
 
-        //hidden ul and text change
+    //sort click event 
+    $("#sort2 ul li").on("click", function(){
         $("#sort2 ul").addClass("hidden");
         sortName = $(this).text();
         $("#sort2 p").text(sortName);
+
+        display(selectedMenu, sortName);
         
-        console.log(selectedMenu);
-        if(sortName == "All"){
-            display(selectedMenu, "all");
-        }else if(sortName == "Low price"){
-            lowPriceItems = selectedMenu.slice();
-            lowPriceItems.sort(lowSorting);
-            display(lowPriceItems, "all");
-        }else if(sortName == "High price"){
-            highPriceItems = selectedMenu.slice();
-            highPriceItems.sort(highSorting);
-            display(highPriceItems, "all");
-        }else if(sortName == "Best"){
-            selectedMenu.map(item => {
-                if(item["popular"] == "best"){
-                    bestItems.push(item);
-                }
-            });
-            display(bestItems, "all");
-            bestItems = [];
-        }
     });
+    //메뉴 클릭 이벤트
+    let lowerTxt;
+    $("#top .menulist ul li").on("click", function(){
+        //Menu name, title change
+        const txt = $(this).text();
+        lowerTxt = txt.toLowerCase();
+        $("#top .menulist p").text(txt);
+        $(".title").text(txt);
+
+        //itemlist change
+        $(".mainlist .items").empty();
+        if(lowerTxt == "all"){
+            selectedMenu = items.filter(item => true);
+            display(selectedMenu, "All");
+            textChange();
+        }else{
+            selectedMenu = items.filter(item => item.type == lowerTxt );
+            selectedMenu.map( item => subMenuList.push(item.sort) );
+            textChange();
+            display(selectedMenu, sortName);
+        }
+        
+
+        $(".sort1").removeClass("active");
+        
+    });
+    //서브메뉴 클릭 이벤트
+    $(".sort1 .submenu").on("click", function(e){
+        const name = e.target.dataset.sub;
+        if(name != undefined){
+            selectedMenu = items.filter(item => (item.sort == name) && (item.type == lowerTxt));
+            display( selectedMenu, sortName);
+            const elSpan = $(".sort1 .submenu span");
+            elSpan.attr("active", "0");
+            for(let i = 0; i < elSpan.length; i++){
+                if( elSpan.eq(i).attr("data-sub") == name ){
+                    elSpan.eq(i).attr("active", "1");
+                }
+            }
+        }
+        
+    });
+    
+
+    //상세페이지 넘어가기
+    $(".mainlist .items li figure a").on("click", function(e){
+        // localStorage.itemid = 
+        e.preventDefault();
+
+    })
 
 
     //end
